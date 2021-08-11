cf3b88509de198c00650466c57d93a39bb87b391
Your Name <you@example.com>
2021-08-11 14:38:40 +0530

    greeting message

---
+++ EmployeWageComputation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EmployeWageComputation
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------------Welcome to Employee Wage Problem--------------------");
+            
+        }
+    }
+}
+
+
+
