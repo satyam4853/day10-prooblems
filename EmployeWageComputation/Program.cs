32588b948f7b059714cb2ef46cd193b5f7ed4882
Your Name <you@example.com>
2021-08-11 14:47:42 +0530

    save the total wage for each company

--- EmployeWageComputation/Program.cs
+++ EmployeWageComputation/Program.cs
@@ -10,9 +10,19 @@ namespace EmployeWageComputation
         {
             Console.WriteLine("----------------Welcome to Employee Wage Problem--------------------");
             
+                    EmployeeClass Tcs = new EmployeeClass("TCS", 77, 30, 150);
+                    EmployeeClass Wipro = new EmployeeClass("Wipro", 57, 27, 100);
+                    Tcs.CompanyEmpWage();
+                    Console.WriteLine(Tcs.ToString());
+                    Wipro.CompanyEmpWage();
+                    Console.WriteLine(Wipro.ToString());
+                }
+            }
         }
-    }
-}
+
+ 
+
+
 
 
 
